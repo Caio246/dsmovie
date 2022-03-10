d609c7e3ba011e450f974084d66f87b58db102e8
Caio <caiostun@gmail.com>
2022-03-10 16:54:32 -0300

    Security config

--- dsmovie/frontend/src/components/Navbar/index.tsx
+++ dsmovie/frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
           <a href="https://github.com/Caio246">
             <div className='dsmovie-contact-container'>
               <GitHubIcon></GitHubIcon>
-              <p className='dsmovie-contact-link'>/devsuperior</p>
+              <p className='dsmovie-contact-link'>/Caio246</p>
             </div>
           </a>
         </div>
