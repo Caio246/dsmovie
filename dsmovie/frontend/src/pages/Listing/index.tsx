245c1d7bcdd94b69bc558a0f33e93413469e3500
Caio <caiostun@gmail.com>
2022-03-10 08:42:45 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
+
+
 /* eslint-disable jsx-a11y/heading-has-content */
 function Listin() {
   return (
-    <h1> Página de listagem</h1>
+    <>
+      <Pagination></Pagination>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div> <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
